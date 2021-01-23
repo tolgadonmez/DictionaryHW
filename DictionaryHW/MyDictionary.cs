ac3dd4f255d7628a7aa8f8919158549a8f5b6037
tolgadonmez <donmeztolga1907@gmail.com>
2021-01-23 16:44:51 +0300

    Add project files.

---
+++ DictionaryHW/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryHW
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKey = keys;
+            TValue[] tempValue = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+            }
+
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public void Print()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("ID => " + keys[i] + " " + "Name => " + values[i]);
+            }
+        }
+    }
+}
---
+++ DictionaryHW/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionaryHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionaries = new MyDictionary<int, string>();
+            dictionaries.Add(16, "Tolga");
+            dictionaries.Add(16, "Eda");
+            dictionaries.Add(67, "Cengiz");
+            dictionaries.Add(06, "Alpay");
+
+            dictionaries.Print();
+            
+
+        }
+    }
+}
